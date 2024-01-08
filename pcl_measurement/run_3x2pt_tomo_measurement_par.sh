a8ce70357fc818cf71c5c2b9334520926c159b67
Jonathan Wong superclass6 <jonathanhw.wong@gmail.com>
2024-01-08 17:46:09 +0000

    Included auxiliary files

---
+++ pcl_measurement/run_3x2pt_tomo_measurement_par.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+start=$SECONDS
+
+PIPELINE_VARIABLES_PATH="/raid/scratch/wongj/mywork/3x2pt/Jonathans_Big_Cosmology_Automator/pcl_measurement/set_variables_3x2pt_measurement.ini"
+export PIPELINE_VARIABLES_PATH
+
+source <(grep = $PIPELINE_VARIABLES_PATH)
+
+echo Creating nz...
+python create_nz_boundaries.py
+echo Done
+
+export REALISATIONS
+echo Measuring power spectra from mock catalogues in parallel...
+mpirun -np 2 python measure_cat_3x2pt_pcls_par.py
+
+echo Calculating Cls averaged over realisations...
+python av_cls.py
+echo Done
+
+echo Running Cosmosis and calculating theoretical 3x2pt spectra...
+
+export PIPELINE_DIR
+export COSMOSIS_ROOT_DIR
+export MEASUREMENT_SAVE_DIR
+export INPUT_ELL_MIN
+export INPUT_ELL_MAX
+export MEASURED_NZ_TABLE_NAME
+
+SAVE_DIR=$MEASUREMENT_SAVE_DIR
+NZ_TABLE_FILENAME=$MEASURED_NZ_TABLE_NAME
+ELL_MAX=$INPUT_ELL_MAX
+ELL_MIN=$INPUT_ELL_MIN
+
+export SAVE_DIR
+export NZ_TABLE_FILENAME
+export ELL_MIN
+export ELL_MAX
+
+cd ${PIPELINE_DIR}/software_utils/
+bash run_cosmosis.sh  &> ${SAVE_DIR}run_cosmosis_log.txt
+echo Done
+cd ${PIPELINE_DIR}/pcl_measurement/
+
+GAUSSIAN_CL_LIKELIHOOD_PATH=${PIPELINE_DIR}gaussian_cl_likelihood/
+export GAUSSIAN_CL_LIKELIHOOD_PATH
+
+echo Measuring Bandpowers from average Cls...
+python measure_cat_bps.py
+echo Done
+
+echo Converting 3x2pt Bps to inference routine input...
+python conv_bps.py
+echo Done
+
+echo Calculating 3x2pt covariance from simulations...
+python cov_fromsim.py
+python cov_fromsim_tot.py
+echo Done
+
+echo Weak Lensing Tomography Pipeline Complete :\)
+
+duration=$((SECONDS-start))
+echo Total time elapsed: $duration
+
+
